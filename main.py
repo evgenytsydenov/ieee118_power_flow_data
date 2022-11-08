2c8e3ca9642241d58fa8c05e4468247e6b05799c
Evgeny Tsydenov <evgenytsydenov@gmail.com>
2022-11-08 19:11:42 +0600

    Update: description of the start point script

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import subprocess
 
 if __name__ == "__main__":
+    """
+    This approach is used to automatically add the project directory to `PYTHONPATH`.
+
+    It is possible to add the project directory to `PYTHONPATH` manually
+     and run `dvc repro` in the terminal.
+    """
 
     # Run all preparation stages
     subprocess.run(["dvc", "repro"])
